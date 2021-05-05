d68a21f7a156819d68c226f6d46e6eae6122490e
rocketshopcode <79589103+rocketshopcode@users.noreply.github.com>
2021-05-05 14:16:36 -0300

    tratamento de excesoes
    dia 05/05/2021

--- curso-exception/pilha.php
+++ curso-exception/pilha.php
@@ -2,7 +2,15 @@
 
 function funcao1(){
     echo 'Entrei na função 1<br>';
-    funcao2();
+    try{
+        funcao2();
+        //$arrayFixo = new SplFixedArray(2);
+        //$arrayFixo[0] = 'valor';
+    } catch(RuntimeException $problema) {
+        echo "Na função 1, eu resolvi o problema da função 2<br>";
+    } catch(DivisionByZeroError)
+        //$divisao = itdiv(5, 0);
+    //funcao2();
     echo 'Saindo da função 1<br>';
 }
 
